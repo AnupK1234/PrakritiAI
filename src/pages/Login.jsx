93b9bc103d57a4e3695bf39f3f8ab85814b42b33
Anish0812 <115338701+Anish0812@users.noreply.github.com>
2023-12-24 11:36:07 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
-import { firebaseAuth } from "../utils/firebase-config";
+import { firebaseAuth } from "../firebase-config";
 
 function Login() {
   const [email, setEmail] = useState("");
