9e8241ee42c144fd493d79fb814baf4974d9a815
Anish0812 <115338701+Anish0812@users.noreply.github.com>
2023-12-24 11:35:09 +0530

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,62 +1,72 @@
-import React, { useState } from 'react'
-import {createUserWithEmailAndPassword,onAuthStateChanged} from 'firebase/auth'
-import { firebaseAuth } from '../utils/firebase-config'
+// Import necessary React and Firebase modules
+import React, { useState } from 'react';
+import { createUserWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
+import { firebaseAuth } from '../utils/firebase-config';
+
 export default function Signup() {
-  const[showPassword,setShowPassword]=useState(false)
-  const[formValues,setFormValues]=useState({
-    email:"",
-    password:"",
-  })
+  const [showPassword, setShowPassword] = useState(false);
+  const [formValues, setFormValues] = useState({
+    email: "",
+    password: "",
+  });
+  
 
-  const handleSigIn=async()=>{
-    try{
-const{email,password} = formValues;
-await createUserWithEmailAndPassword(firebaseAuth,email,password)
+  const handleSigIn = async () => {
+    try {
+      const { email, password } = formValues;
+      await createUserWithEmailAndPassword(firebaseAuth, email, password);
+    } catch (err) {
+      console.log(err);
     }
-    catch(err){
-      console.log(err)
-    }
-  }
-  onAuthStateChanged(firebaseAuth,(currentUser)=>{
-  if(currentUser) navigate('/')})
+  };
+
+
+
   return (
-    <>
-      <div>
+    
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
         <div>
-        <center>
-
-          <h1> We're delighted to have you here! 🌟</h1>
-            <h3> Get ready to embark on a personalized journey to discover your Ayurvedic Prakriti and enhance your well-being.</h3>
-         
-          <h6> Ready to begin your Ayurvedic journey? Click on 'login' if you already have account or else click on get started to created an account</h6>
-          </center>
-
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign up for an account</h2>
         </div>
-        <div>
-        <center>
-      
-          <input type='email' placeholder='Email Address' name='email' value={formValues.email} onChange={(e)=>setFormValues({...formValues,[e.target.name]:e.target.value})}/>
-          </center>
-          <center>
-{showPassword &&(
-          <input type='password' placeholder='Password' name='password' value={formValues.password} onChange={(e)=>setFormValues({...formValues,[e.target.name]:e.target.value})} />)
-}
-          </center>
-          <center>
-
-
-      {!showPassword &&(    <button onClick={()=> setShowPassword(true)}> Get Started</button>)}
-          
-          </center>
-
-
-        </div>
-        <center>
-
-        <button onClick={handleSigIn}> Signup</button>
-        </center>
-
+        <form className="mt-8 space-y-6" onSubmit={handleSigIn}>
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={formValues.email}
+            onChange={(e) => setFormValues({ ...formValues, [e.target.name]: e.target.value })}
+            className="appearance-none rounded-none relative block w-full px-3 py-2 border text-gray-700 leading-5 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+          />
+          <div className="mt-2">
+            <input
+              type={showPassword ? "text" : "password"}
+              placeholder="Password"
+              name="password"
+              value={formValues.password}
+              onChange={(e) => setFormValues({ ...formValues, [e.target.name]: e.target.value })}
+              className="appearance-none rounded-none relative block w-full px-3 py-2 border text-gray-700 leading-5 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+            />
+          </div>
+          <div className="mt-2 flex items-center">
+            <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+              className="text-sm text-green-500 hover:text-blue-700 focus:outline-none"
+            >
+              {showPassword ? "Hide" : "Show"} Password
+            </button>
+          </div>
+          <div className="mt-4">
+            <button
+              type="submit"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-green-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Sign up
+            </button>
+          </div>
+        </form>
       </div>
-    </>
-  )
+    </div>
+  );
 }
