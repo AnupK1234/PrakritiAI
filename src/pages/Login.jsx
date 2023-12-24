19745ce9b5b2e38c316a3ee85c116580cd2c44af
Anish0812 <115338701+Anish0812@users.noreply.github.com>
2023-12-24 11:34:46 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
-
 import { useNavigate } from "react-router-dom";
-
 import { onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
 import { firebaseAuth } from "../utils/firebase-config";
 
@@ -18,36 +16,39 @@ function Login() {
     }
   };
 
-
-
   return (
-    
-      <div className="content">
-      
-       
-            <div className="title">
-              <h3>Login</h3>
-            </div>
-          
-              <input
-                type="text"
-                placeholder="Email"
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-              />
-              <input
-                type="password"
-                placeholder="Password"
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-              />
-              <button onClick={handleLogin}>Login to your account</button>
-            </div>
-         
-      
-  
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-md w-full p-6 bg-white rounded-md shadow-md">
+        <div className="mb-8 text-3xl font-bold text-center text-black-600">Login</div>
+        <div className="mb-4">
+          <input
+            type="text"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring focus:border-indigo-300"
+          />
+        </div>
+        <div className="mb-4">
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring focus:border-indigo-300"
+          />
+        </div>
+        <div className="mb-6">
+          <button
+            onClick={handleLogin}
+            className="w-full px-4 py-2 text-white bg-green-600 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring focus:border-green-300"
+          >
+            Login to your account
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
-
 
 export default Login;
